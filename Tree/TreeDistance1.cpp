ba3dbd09cd6d8abfc2a9bf1ae7b4c28beb3a28a2
Saurabh-1729 <100552672+Saurabh-1729@users.noreply.github.com>
2026-01-31 21:09:35 +0530

    Implement DFS and BFS for tree distance calculation

--- Tree/TreeDistance1.cpp
+++ Tree/TreeDistance1.cpp
@@ -8,3 +8,86 @@ Now we can run the BFS on the above two nodes and can calculate the distance of 
   */
 
 
+ll d = -1, x = -1;
+
+void dfs(ll v, ll parent, ll level, vector<vector<ll>> &adj)
+{
+    for (auto it : adj[v])
+    {
+        if (it != parent)
+        {
+            dfs(it, v, level + 1, adj);
+        }
+    }
+    if (level > d)
+    {
+        d = level;
+        x = v;
+    }
+}
+
+void bfs(ll v, vector<ll> &dis, vector<vector<ll>> &adj)
+{
+    queue<ll> q;
+    q.push(v);
+    dis[v] = 0;
+
+    while (!q.empty())
+    {
+        ll node = q.front();
+        q.pop();
+
+        for (auto it : adj[node])
+        {
+         // Node can be revisited and can get overwritten
+            if (dis[it] == -1) // ✅ VISITED CHECK
+            {
+                dis[it] = dis[node] + 1;
+                q.push(it);
+            }
+        }
+    }
+}
+
+void Saurabh()
+{
+    ll n;
+    cin >> n;
+    vector<vector<ll>> adj(n);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        ll u, v;
+        cin >> u >> v;
+        u -= 1;
+        v -= 1;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    ll a, b; // two fartest node
+    dfs(0, -1, 0, adj);
+    a = x;
+    d = -1;
+    x = -1;
+    dfs(a, -1, 0, adj);
+    b = x;
+    vector<ll> disa(n, -1), disb(n, -1);
+    bfs(a, disa, adj);
+    bfs(b, disb, adj);
+    for (int i = 0; i < n; i++)
+        cout << max(disa[i], disb[i]) << " ";
+    cout << endl;
+}
+
+int main()
+{
+    SPEED();
+    START();
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+        Saurabh();
+    STOP();
+    TIME();
+}
