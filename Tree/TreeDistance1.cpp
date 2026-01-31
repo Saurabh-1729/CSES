1573d973de54ad37f8bbb9ae9874c5351eceda79
Saurabh-1729 <100552672+Saurabh-1729@users.noreply.github.com>
2026-01-31 20:09:58 +0530

    Enhance comments on tree diameter calculation
    Updated comments to clarify the algorithm for finding tree diameter.

--- Tree/TreeDistance1.cpp
+++ Tree/TreeDistance1.cpp
@@ -1,5 +1,10 @@
 /*
 “Every node’s farthest node must be one of the two diameter endpoints.”
+
+1. Pick any node and calculate the fartest distance from that node/
+2. Now the farthest calculaed node will be the starting point of the second dfs, and that will give us the second another farthest node that is the other end of the diametere
+
+Now we can run the BFS on the above two nodes and can calculate the distance of each node from that farthest node.
   */
 
 
